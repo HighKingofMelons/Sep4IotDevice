1669a60267fa95b63ce1cdaf3029dfbd54c51ffb
AdrianaLei <adriana.leisyte@gmail.com>
2023-05-18 00:44:09 +0200

    SEP-53: humidity implementation

--- tests/fakes.c
+++ tests/fakes.c
@@ -96,9 +96,12 @@ DEFINE_FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_measure);
 // int16_t hih8120_getTemperature_x10(void);
 DEFINE_FAKE_VALUE_FUNC(int16_t, hih8120_getTemperature_x10);
 
+// uint16_t hih8120_getHumidityPercent_x10(void);
+DEFINE_FAKE_VALUE_FUNC(uint16_t, hih8120_getHumidityPercent_x10);
 
-// --------------- mh_z19
+// ---------------------------- ~ MH_Z19 ~ ---------------------------------------
+
 DEFINE_FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_getCo2Ppm, int *);
+
 // mh_z19_returnCode_t mh_z19_getCo2Ppm(int *ppm);
 DEFINE_FAKE_VOID_FUNC(mh_z19_initialise, serial_comPort_t);
-DEFINE_FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_takeMeassuring);
