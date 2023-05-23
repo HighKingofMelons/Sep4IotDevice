5da0997bb7c19537fb6b3d83bf294390268e5f93
AdrianaLei <adriana.leisyte@gmail.com>
2023-05-24 00:12:43 +0200

    SEP4-55: edit task config, stack, prio

--- include/taskConfig.h
+++ include/taskConfig.h
@@ -5,29 +5,36 @@
  *  Author: andre
  */ 
 
-
 #pragma once
+
 #include <FreeRTOSConfig.h>
 /* Priorities at which the tasks are created. */
+#define TASK_LORA_UPLINK_PRIORITY   ( tskIDLE_PRIORITY + 3 )
 #define TASK_MESSURE_TEMP_PRIORITY	( tskIDLE_PRIORITY + 2 )
 #define	TASK_MESSURE_HUM_PRIORITY	( tskIDLE_PRIORITY + 2 )
 #define TASK_MESSURE_CO2_PRIORITY	( tskIDLE_PRIORITY + 2 )
 #define TASK_ERROR_PRIORITY         ( tskIDLE_PRIORITY + 1 )
 #define	TASK_ACTUATOR_PRIORITY	    ( tskIDLE_PRIORITY + 1 )
+
 /* Task stack sizes*/
 #define TASK_MESSURE_TEMP_STACK		( configMINIMAL_STACK_SIZE )
 #define	TASK_MESSURE_HUM_STACK		( configMINIMAL_STACK_SIZE )
 #define TASK_MESSURE_CO2_STACK		( configMINIMAL_STACK_SIZE )
+#define TASK_LORA_UPLINK_STACK      ( configMINIMAL_STACK_SIZE + 120 )
 #define TASK_ERROR_STACK            ( configMINIMAL_STACK_SIZE - 80 )
 #define	TASK_ACTUATOR_STACK		    ( configMINIMAL_STACK_SIZE - 100 )
+
 #define configUSE_MUTEXES			1
+
+#define MESURE_CIRCLE_FREAQUENCY    300000UL
+
 /* Initial Limits */
-#define TEMP_MAX_LIMIT 25
-#define TEMP_MIN_LIMIT 18
-#define HUM_MAX_LIMIT 60
-#define HUM_MIN_LIMIT 30
-#define CO2_MAX_LIMIT 900
-#define CO2_MIN_LIMIT 100
+#define TEMP_MAX_LIMIT              25
+#define TEMP_MIN_LIMIT              18
+#define HUM_MAX_LIMIT               60
+#define HUM_MIN_LIMIT               30
+#define CO2_MAX_LIMIT               900
+#define CO2_MIN_LIMIT               0
 
 #define DEBUG                       1
 #define DEBUG_ERROR                 0
