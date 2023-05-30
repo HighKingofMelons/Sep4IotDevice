3dfba6ed3ee914bf7289786696bf4d69391739ed
Nerijus <takataole@gmail.com>
2023-05-30 16:58:01 +0200

    Added humidity function descriptions.

--- include/humidity.h
+++ include/humidity.h
@@ -25,9 +25,39 @@
  * It hold a 10 attributes and is needed for accesing and using co2 values.
  */
 typedef struct humidity* humidity_t;
+/**
+ * @brief Create humidity.
+ *
+ * @param mesureCirclefreequency
+ * @return humidity_t
+ */
 humidity_t humidity_create(TickType_t mesureCircleFreequency);
+/**
+ * @brief Gets the latest average humidity value.
+ *
+ * @param self
+ * @return uint8_t
+ */
 uint8_t humidity_get_latest_average_humidity(humidity_t self);
+/**
+ * @brief Sets the humidity limits.
+ *
+ * @param self
+ * @param maxLimit
+ * @param minLimit
+ */
 void humidity_set_limits(humidity_t self, uint8_t maxLimit, uint8_t minLimit);
+/**
+ * @brief Checks the humidity acceptability status and returns the status.
+ *
+ * @param self
+ * @return int8_t
+ */
 int8_t humidity_acceptability_status(humidity_t self);
+/**
+ * @brief Destrys the humidity struct.
+ *
+ * @param self
+ */
 void humidity_destroy(humidity_t self);
 
