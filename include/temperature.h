806333a9337db81e08c8494b2645409e24bf65be
Nerijus <takataole@gmail.com>
2023-05-30 17:04:44 +0200

    Addec temperature function descriptions.

--- include/temperature.h
+++ include/temperature.h
@@ -18,9 +18,39 @@
  * It hold a 10 attributes and is needed for accesing and using co2 values.
  */
 typedef struct temperature* temperature_t;
+/**
+ * @brief Create temperature.
+ *
+ * @param mesureCirclefreequency
+ * @return temperature_t
+ */
 temperature_t temperature_create(TickType_t mesureCircleFreequency);
+/**
+ * @brief GEts the latest average temperature value.
+ *
+ * @param self
+ * @return int16_t
+ */
 int16_t temperature_get_latest_average_temperature(temperature_t self);
+/**
+ * @brief Sets the temperature limits.
+ *
+ * @param self
+ * @param maxLimit
+ * @param minLimit
+ */
 void temperature_set_limits(temperature_t self, int16_t maxLimit, int16_t minLimit);
+/**
+ * @brief Checks the temperature acceptability status and returns the status.
+ *
+ * @param self
+ * @return int8_t
+ */
 int8_t temperature_acceptability_status(temperature_t self);
+/**
+ * @brief Destrys the temperature struct.
+ *
+ * @param self
+ */
 void temperature_destroy(temperature_t self);
 
