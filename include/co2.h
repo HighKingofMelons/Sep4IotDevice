cdeb2fe37fb43420f788b0ece188473d7305cdf4
Nerijus <takataole@gmail.com>
2023-05-30 16:46:58 +0200

    Added co2 function descripions.

--- include/co2.h
+++ include/co2.h
@@ -33,13 +33,71 @@ co2_t co2_create(TickType_t freequency);
  * @param pvParameters
  */
 void co2_mesure(void *pvParameters);
+/**
+ * @brief Get the latest average co2 value.
+ *
+ * @param self
+ * @return int16_t
+ */
 int16_t co2_get_latest_average_co2(co2_t self);
+/**
+ * @brief Initializes the co2 driver.
+ *
+ * @return int
+ */
 int initializeCo2Driver();
+/**
+ * @brief Makes one co2 measurement.
+ *
+ * @param self
+ * @return int
+ */
 int makeOneCo2Mesurment(co2_t self);
+/**
+ * @brief Add co2 value to the co2.
+ *
+ * @param self
+ * @param co2
+ */
 void addCo2(co2_t self, int16_t co2);
+/**
+ * @brief Resets the co2 array.
+ *
+ * @param self
+ */
 void resetCo2Array(co2_t self);
+/**
+ * @brief Calculates co2.
+ *
+ * @param self
+ */
 void calculateCo2(co2_t self);
+/**
+ * @brief Sets the co2 limits.
+ *
+ * @param self
+ * @param maxLimit
+ * @param minLimit
+ */
 void co2_set_limits(co2_t self, int16_t maxLimit, int16_t minLimit);
+/**
+ * @brief Checks co2 acceptability statys and returns the status.
+ *
+ * @param self
+ * @return int8_t
+ */
 int8_t co2_acceptability_status(co2_t self);
+/**
+ * @brief Records the co2 measurement.
+ *
+ * @param self
+ * @param ppm
+ */
 void co2_recordMeasurement(co2_t self, uint16_t ppm);
+/**
+ * @brief Makes one co2 measurement.
+ *
+ * @param self
+ * @param ppm
+ */
 void co2_makeOneMesuremnt(co2_t self, uint16_t ppm);
