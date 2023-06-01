f2e899b8ff3df8895676f2c6e85c26a55a90dc79
Nerijus <takataole@gmail.com>
2023-06-01 05:26:02 +0200

    Doxygen descriptions and generation.

--- include/actuation.h
+++ include/actuation.h
@@ -1,8 +1,28 @@
+/** @file
+ * @brief This is a actuation header file.
+ *
+ * It handles the actuation.
+ */
 #pragma once
-
+/**
+ * @brief Structure to store actuation information.
+ *
+ * It hold a 5 attributes and is needed for accesing and using actuation.
+ */
 typedef struct actuation_handler *actuation_handler_t;
-
+/**
+ * @brief Initiatizes the actuation handler.
+ *
+ * @param temperature
+ * @param humidity
+ * @return actuation_handler_t
+ */
 actuation_handler_t actuation_handler_init(temperature_t temperature, humidity_t humidity);
+/**
+ * @brief Destroys the actuation handler.
+ *
+ * @param victim
+ */
 void actuation_handler_destroy(actuation_handler_t victim);
 
 typedef enum {
@@ -12,7 +32,19 @@ typedef enum {
 /*
     Takes pdTRUE for ON and pdFALSE for OFF
 */
+
+/**
+ * @brief Ventilation actuators overide state.
+ *
+ * @param self
+ * @param state
+ */
 void actuators_ventilation_override_state(actuation_handler_t self, vent_state_t state);
+/**
+ * @brief Ventilation actuators disable state.
+ *
+ * @param self
+ */
 void actuators_ventilation_disable_override(actuation_handler_t self);
 
 typedef enum {
@@ -25,8 +57,18 @@ typedef enum {
     ACTUATORS_ON =  1,
     ACTUATORS_OFF = 0,
 } actuator_state_t;
-
+/**
+ * @brief Ventilation actuators overide state.
+ *
+ * @param self
+ * @param state
+ */
 void actuators_aircon_override_state(actuation_handler_t self, aircon_state_t state);
+/**
+ * @brief Ventilation actuators disable state.
+ *
+ * @param self
+ */
 void actuators_aircon_disable_override(actuation_handler_t self);
 
 void actuators_turn_on_off(actuation_handler_t self, actuator_state_t state);
--- include/error_handler.h
+++ include/error_handler.h
@@ -1,3 +1,9 @@
+/** @file
+ * @brief This is a error header file.
+ *
+ * It handles the errors.
+ */
+
 #pragma once
 
 typedef enum {
@@ -10,14 +16,46 @@ typedef enum {
 } error_component_t;
 
 typedef uint8_t error_flags_t;
+/**
+ * @brief Structure to store error information.
+ *
+ * It hold a 5 attributes and is needed for accesing and using error.
+ */
 typedef struct error_handler *error_handler_t;
-
+/**
+ * @brief Initiatizes the error handler.
+ *
+ * @return error_handler_t
+ */
 error_handler_t error_handler_init();
-
+/**
+ * @brief The error handler report function.
+ *
+ * @param self
+ * @param componenet
+ * @return BaseType_t
+ */
 BaseType_t error_handler_report(error_handler_t self, error_component_t component);
+/**
+ * @brief The error handler revoke function.
+ *
+ * @param self
+ * @param componenet
+ * @return BaseType_t
+ */
 BaseType_t error_handler_revoke(error_handler_t self, error_component_t component);
 
 /* The first 3 bits of the return are always 0 */
+/**
+ * @brief The error handler get flags function.
+ *
+ * @param self
+ * @return error_flags_t
+ */
 error_flags_t error_handler_get_flags(error_handler_t self);
-
+/**
+ * @brief Destroys the error handler.
+ *
+ * @param self
+ */
 void error_handler_destroy(error_handler_t self);
